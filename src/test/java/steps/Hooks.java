bc62761f76114509e2ecb0d13d36a4c16c70158e
vm31 <vmiddela@gmail.com>
2021-09-14 19:01:59 +0100

    initial commit

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -16,7 +16,7 @@ public class Hooks extends BaseTest {
         System.setProperty("webdriver.chrome.driver",util.readPropertyFile("CHROME_PATH"));
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
 
     }
