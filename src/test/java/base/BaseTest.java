120f1312cf7efd8b3283aa0aef4e0c1a74ab4704
vaishnavi-middela <vaishnavi-middela@koushiks-mbp.home>
2021-10-27 21:37:26 +0200

    removed unwanted code

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,42 +1,21 @@
 package base;
 
 import helper.Utility;
-import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import pages.*;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-import java.util.logging.Logger;
-
 public class BaseTest {
     protected HomePage homePageObj;
     public static WebDriver driver;
-    protected HeaderIconsPage headerIconsPageObj;
-    protected LoginPage loginPageObj;
-    protected SearchPage searchPageObj;
     protected Utility util;
-
-
-
-
-
 
     public BaseTest(){
         homePageObj= PageFactory.initElements(driver, HomePage.class);
-        headerIconsPageObj=PageFactory.initElements(driver, HeaderIconsPage.class);
-        loginPageObj=PageFactory.initElements(driver, LoginPage.class);
-        searchPageObj=PageFactory.initElements(driver, SearchPage.class);
         util=PageFactory.initElements(driver, Utility.class);
 
 
     }
 
-
-    public void getLogs(){
-
-    }
 
 }
