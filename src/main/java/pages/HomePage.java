98202f726e205271d8cf0e79f912af8fe3d07f2c
vaishnavi-middela <vaishnavi-middela@koushiks-mbp.home>
2021-11-20 16:04:30 +0100

    resolve conflict

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,15 +1,8 @@
 package pages;
 
-import cucumber.api.java.en.Then;
-import org.openqa.selenium.*;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.Assert;
 
-import java.security.Signature;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage {
     //variable
